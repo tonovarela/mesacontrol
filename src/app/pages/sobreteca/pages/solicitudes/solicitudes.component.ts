f1b5d1a10c23b7fa77991af8d1f84b124d7e1c39
tonovarela <tonovarela@live.com>
2025-10-09 17:32:54 -0600

    feat: agregar lógica para selección de órdenes y mostrar alerta si no hay gaveta asignada

--- src/app/pages/sobreteca/pages/solicitudes/solicitudes.component.ts
+++ src/app/pages/sobreteca/pages/solicitudes/solicitudes.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component, computed, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BaseGridComponent } from '@app/abstract/BaseGrid.component';
+import { OrdenMetrics } from '@app/interfaces/responses/ResponseOrdenMetrics';
 import { TypeSearchMetrics } from '@app/interfaces/type';
 import { SynfusionModule } from '@app/lib/synfusion.module';
+import { UiService } from '@app/services';
 import { SearchMetricsComponent } from '@app/shared/search-metrics/search-metrics.component';
 
 @Component({
@@ -14,6 +16,7 @@ import { SearchMetricsComponent } from '@app/shared/search-metrics/search-metric
 })
 export default class SolicitudesComponent extends BaseGridComponent implements OnInit { 
   protected minusHeight = 0.3;
+  private uiService= inject(UiService);
    private _activatedRouter = inject(ActivatedRoute);
    public titulo = computed(() => this._activatedRouter.snapshot.data['titulo']);
    public readonly type = TypeSearchMetrics.SOBRESPREPRENSA
@@ -22,5 +25,18 @@ export default class SolicitudesComponent extends BaseGridComponent implements O
   }
   ngOnInit(): void {
       this.iniciarResizeGrid(this.minusHeight, true);
+  }
+
+  public async onSelectOrder(orden: OrdenMetrics | null) {
+    if (!orden) {
+      return;
+    }
+    console.log('Orden seleccionada:', orden);
+    const tieneGaveta = orden?.no_gaveta && orden.no_gaveta > 0;
+    if (!tieneGaveta) {
+        this.uiService.mostrarAlertaError('',`El sobre con la orden ${orden.NoOrden} no tiene gaveta asignada, por favor asignar antes de continuar`);
+      return;
+    }
+  
   }
 }
