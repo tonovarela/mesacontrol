3e05dd6a9cecb8b1aaa208b006feba76addfeaaf
tonovarela <tonovarela@live.com>
2025-10-16 10:39:58 -0600

    feat: implementar componente de diálogo para la gestión de detalles y préstamos

--- src/app/pages/sobreteca/componentes/sobre-detalle/sobre-detalle.component.ts
+++ src/app/pages/sobreteca/componentes/sobre-detalle/sobre-detalle.component.ts
@@ -1,10 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, output, ViewChild } from '@angular/core';
+import { PrimeModule } from '@app/lib/prime.module';
 
 @Component({
-  selector: 'app-sobre-detalle',
-  imports: [],
+  selector: 'sobre-detalle',
+  imports: [CommonModule,PrimeModule],
   templateUrl: './sobre-detalle.component.html',
   styleUrl: './sobre-detalle.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SobreDetalleComponent { }
+export class SobreDetalleComponent {
+
+  @ViewChild('dialogModal') dialogModal: any;
+
+  public tieneOrdenSeleccionada = input<boolean>(false);
+  public ordenActual = input<any | null>(null);
+  public componentesAgrupados = input<any[]>([]);
+  public onCerrarDetalle = output<void>();
+  public onDevolverPrestamo = output<void>();
+  public onSolicitarPrestamo = output<void>();
+
+
+
+  cerrarDetalle(){
+    this.onCerrarDetalle.emit();
+  }
+
+  devolverPrestamo(){
+    this.onDevolverPrestamo.emit();
+  }
+
+  solicitarPrestamo(){
+    this.onSolicitarPrestamo.emit();
+  }
+
+}
