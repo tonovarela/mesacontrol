6c2c5619da1091b283a31ee2919a36c087a8f334
tonovarela <tonovarela@live.com>
2026-09-07 10:27:58 -0600

    feat: actualizar íconos y estilos en el sidebar; agregar nuevos SVGs para secciones de preprensa y producción

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -27,19 +27,19 @@ export class SidebarComponent   implements OnInit {
     {
       nombre: 'Pendientes',
       claseSize: 'w-8 h-8 svg-icon',
-      icono: 'assets/img/orders.svg',
+      icono: 'assets/img/sidebar/preprensa-pendientes.svg',
       path: '/preprensa/pendientes'
     },
     {
       nombre: 'Liberadas',
       claseSize: 'w-8 h-8 svg-icon',
-      icono: 'assets/img/liberadas.svg',
+      icono: 'assets/img/sidebar/preprensa-liberadas.svg',
       path: '/preprensa/liberadas'
     },
     {
       nombre: 'Omisiones',
       claseSize: 'w-8 h-8 svg-icon',
-      icono: 'assets/img/liberadas.svg',      
+      icono: 'assets/img/sidebar/preprensa-omisiones.svg',
       path: '/preprensa/omisiones'
     }
   ]);
@@ -47,20 +47,20 @@ export class SidebarComponent   implements OnInit {
   public rutasProduccion= signal<Ruta[]>([
     {
       nombre: 'Pendientes',
-      icono: 'assets/img/orders.svg',
+      icono: 'assets/img/sidebar/produccion-pendientes.svg',
       path: '/produccion/pendientes',
       claseSize: 'w-8 h-8 svg-icon',
     },
     {
       nombre: 'Liberadas',
-      icono: 'assets/img/liberadas.svg',
+      icono: 'assets/img/sidebar/produccion-liberadas.svg',
       path: '/produccion/liberadas',
       claseSize: 'w-8 h-8 svg-icon',
     },
     {
       nombre: 'Procesos faltantes',
       claseSize: 'w-8 h-8 svg-icon',
-      icono: 'assets/img/liberadas.svg',      
+      icono: 'assets/img/sidebar/produccion-faltantes.svg',
       path: '/produccion/omisiones'
     }
     
@@ -69,40 +69,40 @@ export class SidebarComponent   implements OnInit {
   public rutasControlElementos =   signal<Ruta[]>([
     {
       nombre: 'Solicitudes',
-      icono: 'assets/img/control.svg',
+      icono: 'assets/img/sidebar/control-solicitudes.svg',
       path: '/control_elementos/solicitudes',
       claseSize: 'w-8 h-8 svg-icon',
     },
     {
       nombre: 'Histórico',
-      icono: 'assets/img/historico.svg',
+      icono: 'assets/img/sidebar/control-historico.svg',
       path: '/control_elementos/historico',
-      claseSize: 'w-7 h-7 svg-icon',
+      claseSize: 'w-8 h-8 svg-icon',
     }
   ]);
 
   public rutasSobreteca = signal<Ruta[]>([
     {
       nombre: 'Revisión  sobres',
-      icono: 'assets/img/sobre.svg',
+      icono: 'assets/img/sidebar/sobreteca-revision.svg',
       path: '/sobreteca/sobres',
       claseSize: 'w-8 h-8 svg-icon',
     },
     {
       nombre: 'Sin gaveta asignada',
-      icono: 'assets/img/sobre-confirmado.svg',
+      icono: 'assets/img/sidebar/sobreteca-sin-gaveta.svg',
       path: '/sobreteca/sobres-confirmados',
       claseSize: 'w-8 h-8 svg-icon',
     },
     {
       nombre: 'Prestamo',
-      icono: 'assets/img/prestamo.svg',
+      icono: 'assets/img/sidebar/sobreteca-prestamo.svg',
       path: '/sobreteca/solicitudes',
       claseSize: 'w-8 h-8 svg-icon',
     },
     {
       nombre: 'Histórico',
-      icono: 'assets/img/historico-prestamo.svg',
+      icono: 'assets/img/sidebar/sobreteca-historico.svg',
       path: '/sobreteca/historico',
       claseSize: 'w-8 h-8 svg-icon',
     }
